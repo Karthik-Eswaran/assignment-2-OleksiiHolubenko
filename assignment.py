5b5708255a32effa94f56e2563421e9265a659de
OleksiiHolubenko <oleksii.holubenko@student.uj.edu.pl>
2025-05-04 17:29:07 +0200

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,34 +1,35 @@
-def read_file(file_path: str) -> str:
-    """
-    Reads the contents of a file and returns it as a string.
-    """
-    raise NotImplementedError()
+def read_file(file_path: str) -> str: 
+    try:
+         f = open(file_path, "r")
+         return f.read()
+    except FileNotFoundError:
+         return "File not found" 
 
 
 def write_file(file_path: str, content: str) -> None:
-    """
-    Writes the given content to a file.
-    """
-    raise NotImplementedError()
+    f = open(file_path, "w")
+    f.write(content)
 
+import os
 
 def list_files_in_directory(directory_path: str) -> list:
-    """
-    Returns a list of files in the specified directory.
-    """
-    raise NotImplementedError()
+    try:
+        return os.listdir(directory_path)
+    except FileNotFoundError:
+        return "File not found"
 
 
 def generate_numbers(n: int) -> iter:
-    """
-    Generates a sequence of numbers from 0 to n-1 using an iterator.
-    """
-    raise NotImplementedError()
-
+    return iter(range(n))
+    
+import importlib
 
 def use_function_from_module(module_name: str, function_name: str, *args) -> any:
-    """
-    Demonstrates how to import a function from another script (module) and execute it.
-    The module name and function name are passed as strings, along with any arguments for the function.
-    """
-    raise NotImplementedError()
+    try:
+        module = importlib.import_module(module_name)  
+        func = getattr(module, function_name)         
+        return func(*args)                            
+    except ModuleNotFoundError:
+        return(f"Module '{module_name}' not found")
+    except AttributeError:
+        return(f"Attribute '{function_name}' not Found")
